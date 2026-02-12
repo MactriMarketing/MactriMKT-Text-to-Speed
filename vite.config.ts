9d89214786d44c0aea41bb5548b453028e0d2aa1
MactriMarketing <trimedia1997@gmail.com>
2026-02-12 15:03:48 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+        base: '/MactriMKT-Text-to-Speed/',
       server: {
         port: 3000,
         host: '0.0.0.0',
